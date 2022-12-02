4a4abc2dc11de8e7ba44065a3b95331d07e89b59
abdmrknjc <mrkonjicabdullah08@gmail.com>
2022-12-02 22:20:55 +0100

    started z001

--- Z001/Z001/Z001.cpp
+++ Z001/Z001/Z001.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
-/* 30.01.2021.
+// 30.01.2021.
+
+/*Napisati program koji korisniku omogu?ava unos realnog broja x
+i odabir funkcije za prora?un datog izraza. Korisnik bira funkciju unosom sljede?ih karaktera 's' (sin), 'c' (cos).
+U slu?aju da korisnik unese karakter koji ne odgovara niti jednoj od predefinisanih vrijednosti ispisati odgovaraju?u poruku i ponoviti odabir.
+
+Za odabranu funkciju sin izraz je:
+
+
 */
+
+
+
 
 int main() {
 	return 0;
