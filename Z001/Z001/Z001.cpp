0e79b1c96b431354efdf3475309b4301f091c93d
abdmrknjc <mrkonjicabdullah08@gmail.com>
2022-12-02 22:37:42 +0100

    finished Z001

--- Z001/Z001/Z001.cpp
+++ Z001/Z001/Z001.cpp
@@ -9,12 +9,36 @@ U slucaju da korisnik unese karakter koji ne odgovara niti jednoj od predefinisa
 
 Za odabranu funkciju sin izraz je:
 
+f(x,m)=1 + 1/((sin(x) + x) + 2/(sin(x/2)) + x + ... + m/(sin(x/m)) + x
 
 */
 
+float unosBroja() {
+	float broj;
+	cout << "Unesite realan broj x: " << endl;
+	cin >> broj;
+	return broj;
+}
 
 
+float izracun(float broj) {
+	char izbor;
+	while (cout << "Izaberte funkciju s - sin, c - cos: ", cin >> izbor, izbor != 's' && izbor != 'c')
+		cout << "Odabrali ste nepostojucu funkciju" << endl;
+
+	float rezultat = 0;
+	if (izbor == 's')
+		for (int i = 1; i <= broj; i++) rezultat += i / (sin(broj / i) + broj);
+	else
+		for (int i = 1; i <= broj; i++) rezultat += i / (cos(broj / i) + broj);
+
+	return rezultat;
+
+}
 
 int main() {
+	float x = unosBroja();
+	cout << "Rezultat je: " << izracun(x);
+
 	return 0;
 }
